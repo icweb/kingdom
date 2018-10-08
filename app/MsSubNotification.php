bcf02783eadd9ae2794408ba511003fa698153ed
Ian <ianconway@protonmail.com>
2018-10-08 19:20:28 -0400

    Updated to record notifications received

--- app/MsSubNotification.php
+++ app/MsSubNotification.php
@@ -16,6 +16,23 @@ class MsSubNotification extends Model
 
     public function produce($data)
     {
+        //        array (
+//            'changeType' => 'updated',
+//            //'clientState' => 'bed9a2d4050b19edf8e8657f92fc50bc',
+//            'resource' => 'Users/e3752196-b107-4231-bb10-8881777ca031',
+//            'resourceData' =>
+//                array (
+//                    '@odata.type' => '#Microsoft.Graph.Group',
+//                    '@odata.id' => 'Groups/e3752196-b107-4231-bb10-8881777ca031',
+//                    'id' => 'e3752196-b107-4231-bb10-8881777ca031',
+//                    'organizationId' => 'ebe6fd27-0c10-44bb-a3fc-213e729cb8c9',
+//                    'sequenceNumber' => 636746356026729019,
+//                ),
+//            'subscriptionExpirationDateTime' => '2018-10-10T22:21:39+00:00',
+//            //'subscriptionId' => 'bbd1c8dd-a2a9-4974-a4b3-1cd43b465027',
+//            'tenantId' => 'ebe6fd27-0c10-44bb-a3fc-213e729cb8c9',
+//        ),
+
         foreach($data['value'] as $item)
         {
             $ms_sub = MsSubscription::where([
@@ -34,11 +51,8 @@ class MsSubNotification extends Model
                     $resource_type = 'GROUP';
                 }
 
-                foreach($item['resourceData'] as $itemData)
-                {
-                    $resource = new MsResource();
-                    $resource->getOrCreate($itemData['id'], $resource_type);
-                }
+                $resource = new MsResource();
+                $resource->getOrCreate($item['resourceData']['id'], $resource_type);
             }
         }
     }
