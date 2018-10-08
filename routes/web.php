d6717bece2e4983019933d7454d1cf1009abe31a
Ian <ianconway@protonmail.com>
2018-10-08 18:16:10 -0400

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,6 @@ Route::get('/createusersub', function () {
 
 Route::post('/webhooks', function (\Illuminate\Http\Request $request) {
 
-    info('webhooks hit');
-
-    info($request->all());
-
     if(isset($_GET['validationToken']))
     {
         return response($_GET['validationToken'], 200)->header('Content-Type', 'text/plain');
