9292767a8eef086f345c91cf4b6933c652fb8463
Ian <ianconway@protonmail.com>
2018-10-08 21:24:21 -0400

    added fields back

--- app/MsResource.php
+++ app/MsResource.php
@@ -93,7 +93,7 @@ class MsResource extends Model
 
     public function getOrCreate($ms_id, $type = 'USER', $changeType)
     {
-        if($changeType === 'deleted')
+        if($changeType !== 'deleted')
         {
             $data = ['ms_id' => $ms_id, 'type'  => $type];
 
@@ -165,13 +165,13 @@ class MsResource extends Model
                 $data['mailNickname'] = $group->getMailNickname();
                 $data['mail'] = $group->getMail();
                 $data['classification'] = $group->getClassification();
-                //$data['createdDateTime'] = $group->getCreatedDateTime()->format('Y-m-d H:i:s');
+                $data['createdDateTime'] = $group->getCreatedDateTime()->format('Y-m-d H:i:s');
                 $data['groupTypes'] = $group->getGroupTypes();
-                //$data['onPremisesLastSyncDateTime'] = $group->getOnPremisesLastSyncDateTime()->format('Y-m-d H:i:s');
+                $data['onPremisesLastSyncDateTime'] = $group->getOnPremisesLastSyncDateTime()->format('Y-m-d H:i:s');
                 $data['onPremisesSecurityIdentifier'] = $group->getOnPremisesSecurityIdentifier();
                 $data['onPremisesSyncEnabled'] = $group->getOnPremisesSyncEnabled() ? 'Yes' : 'No';
                 $data['proxyAddresses'] = $group->getProxyAddresses();
-                //$data['renewedDateTime'] = $group->getRenewedDateTime()->format('Y-m-d H:i:s');
+                $data['renewedDateTime'] = $group->getRenewedDateTime()->format('Y-m-d H:i:s');
                 $data['securityEnabled'] = $group->getSecurityEnabled() ? 'Yes' : 'No';
                 $data['visibility'] = $group->getVisibility();
                 $data['allowExternalSenders'] = $group->getAllowExternalSenders() ? 'Yes' : 'No';
