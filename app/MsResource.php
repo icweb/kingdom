1dcc617f831447459d1bde95cbcad2ffab214049
Ian <ianconway@protonmail.com>
2018-10-09 20:14:25 -0400

    added birthday

--- app/MsResource.php
+++ app/MsResource.php
@@ -221,14 +221,16 @@ class MsResource extends Model
 
                 if(isset($mailboxSettings))
                 {
+                    info([$mailboxSettings->getAutomaticRepliesSetting()->getExternalAudience()]);
+
                     MsMailboxSetting::create([
                         'resource_id'               => $ms_resource->id,
-                        'externalAudience'          => $mailboxSettings->getAutomaticRepliesSetting()->getExternalAudience()->_value,
+                        //'externalAudience'          => $mailboxSettings->getAutomaticRepliesSetting()->getExternalAudience()->_value,
                         'externalReplyMessage'      => $mailboxSettings->getAutomaticRepliesSetting()->getExternalReplyMessage(),
                         'internalReplyMessage'      => $mailboxSettings->getAutomaticRepliesSetting()->getInternalReplyMessage(),
                         'scheduledEndDateTime'      => $mailboxSettings->getAutomaticRepliesSetting()->getScheduledEndDateTime()->getDateTime(),
                         'scheduledStartDateTime'    => $mailboxSettings->getAutomaticRepliesSetting()->getScheduledStartDateTime()->getDateTime(),
-                        'status'                    => $mailboxSettings->getAutomaticRepliesSetting()->getStatus()->_value,
+                        //'status'                    => $mailboxSettings->getAutomaticRepliesSetting()->getStatus()->_value,
                     ]);
                 }
             }
