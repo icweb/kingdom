d11b0c5781fc58f62ec4acd8182b34646b2abd0b
Ian <ianconway@protonmail.com>
2018-10-14 15:26:23 -0400

    added manager route for testing

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/getmanagerinfo', function () {
     $graph = $graph->setAccessToken(\App\Token::fetch());
 
     $user = $graph
-        ->createRequest("GET", '/users/iconway@wcsi.org?$select=manager')
+        ->createRequest("GET", '/users/iconway@wcsi.org?$expand=children')
         ->setReturnType(\Microsoft\Graph\Model\User::class)
         ->execute();
 
